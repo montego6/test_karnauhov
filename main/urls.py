7412d507bf2d63fac8cd0a9d5a8f0e900e9afe84
Kirill Kirsanov <montego@yandex.ru>
2023-09-14 19:08:55 +0300

    Swagger added

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import ConvertApiView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('rates/', ConvertApiView.as_view(), name='api-convert'),
+     path(
+        "docs/",
+        TemplateView.as_view(template_name="swagger-ui.html"),
+        name="swagger-ui",
+    ),
+
 ]
